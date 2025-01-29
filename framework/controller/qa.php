d9a872f8b7089f9c4ce7ec49a9383f9ce2529984
kinoppol <kinoppol@hotmail.com>
2025-01-29 23:37:36 +0700

    evd

--- framework/controller/qa.php
+++ framework/controller/qa.php
@@ -5,7 +5,12 @@ class qa{
         $oqasModel = model('oqas_model');
         $indicator=$oqasModel->get_indicators(['id'=>$param['ind']]);
         $data['title']=$indicator[0]['title'];
-        $data['content']=$indicator[0]['subject'];
+        $data['subject']=$indicator[0]['subject'];
+        $data['content']=view('oqas/evidence',$data);
+        return view('_template/main',$data);
+    }
+    function evidence_form(){
+        $data['content']=view('oqas/evidence_form');
         return view('_template/main',$data);
     }
 }
---
+++ framework/view/oqas/evidence.php
@@ -0,0 +1,5 @@
+<a href="<?php
+                      print site_url('qa/evidence_form'); ?>"
+                          class="btn btn-primary">
+                          เพิ่มหลักฐานประกอบ
+                      </a>
